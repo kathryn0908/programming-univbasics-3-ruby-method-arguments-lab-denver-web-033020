7ae1348787bd2fec9efe9e1c304106625f2a7c9a
Kathryn Clark <kathrynlael@gmail.com>
2020-03-04 00:56:27 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -15,7 +15,7 @@
   def introduction_with_language_optional(name, language="Ruby")
     puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
   end
-  introduction_with_language_optional("Edwin",language)
+  introduction_with_language_optional("Edwin")
   introduction_with_language_optional("Edwin", "Python")
   
   
